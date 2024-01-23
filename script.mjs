fac403824aadcdf688f69a90f571ffff5797a8d8
Afsheen Mujahid <144379396+AfsheenMujahid@users.noreply.github.com>
2024-01-23 21:02:42 +0500

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -1,25 +1,19 @@
-const submit = document.getElementById("submit");
-const locationInput = document.getElementById("location");
-const weather = document.getElementById("weather");
-
-const apiKey = "c18ac18f95f5429b80a132952241701";
-
 function fetchWeather() {
     const locationValue = locationInput.value;
-     if (locationValue) {
-                const apiUrl = `http://api.weatherapi.com/v1/current.json?key=c18ac18f95f5429b80a132952241701&q=Karachi&aqi=no`;
+    if (locationValue) {
+        const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${locationValue}&aqi=no`;
 
-                fetch(apiUrl)
-                    .then(response => response.json())
-                    .then(data => {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
                 const temp = data.current.temp_c;
-                 const description = data.current.condition.text;
+                const description = data.current.condition.text;
                 const locationName = data.location.name;
- 
+
                 weather.innerHTML = `
                     <h2>${locationName}</h2>
                     <p>Temperature ${temp}°C - ${description}</p>
-                   `;
+                `;
             })
             .catch(error => {
                 console.error("Error fetching weather data:", error);
@@ -29,12 +23,3 @@ function fetchWeather() {
         weather.innerHTML = "<p>Please enter a location.</p>";
     }
 }
-
-submit.addEventListener("click", fetchWeather);
-
-// Add event listener for Enter key press
-locationInput.addEventListener("keyup", (event) => {
-    if (event.key === "Enter") {
-        fetchWeather();
-    }
-});
